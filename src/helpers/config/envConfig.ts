b9760b7d8634cef79644797163b2cd00dade9f46
akram-sakib <sayedakramsakib@gmail.com>
2023-11-09 17:32:41 +0600

    first commit

---
+++ src/helpers/config/envConfig.ts
@@ -0,0 +1,7 @@
+export const getBaseUrl = (): string => {
+  return process.env.BACKEND_URL || "http://localhost:5000/api/v1";
+};
+
+export const envConfig = {
+  siteUrl: process.env.NEXTAUTH_URL || "http://localhost:3000",
+};
